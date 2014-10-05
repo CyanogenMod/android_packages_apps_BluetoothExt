b245692cacaf255e0573d09a0d0da803691e4ea5
Tony Layher <layhertony@gmail.com>
2014-10-05 00:54:17 -0400

    Handle multiple paths in the SECONDARY_STORAGE variable.
      Some devices list multiple paths for the SECONDARY_STORAGE
      variable, I.E. /storage/sdcard1:/storage/usbdisk
      When attempting to browse the device using the BT Ftp protocol
      accessing EXTERNAL STORAGE would result in failed path
      being mapped, and cause the system to fall into a loop.
    
      Its also assumed that the device will have only one real
      SECONDARY_STORAGE with the rest being linked in some
      manner back to the first.
    
    Change-Id: I3edf73f2b7e31cbaa84cc9c5f22a49c9ffb12347

--- src/org/codeaurora/bluetooth/ftp/BluetoothFtpObexServer.java
+++ src/org/codeaurora/bluetooth/ftp/BluetoothFtpObexServer.java
@@ -211,7 +211,9 @@ public class BluetoothFtpObexServer extends ServerRequestHandler {
         msg.sendToTarget();
         /*Initialize the internal, external storage root paths from Enivronment*/
         rootPrimaryStoragePath = Environment.getExternalStorageDirectory().getPath();
-        rootSecondaryStoragePath = System.getenv(ENV_SECONDARY_EXTERNAL_STORAGE);
+        String secondaryStorage = System.getenv(ENV_SECONDARY_EXTERNAL_STORAGE);
+        String[] secondaryStorageList = secondarystorage.split(":");
+        rootSecondaryStoragePath = secondarystoragelist[0];
         /*Initialize mCurrentPath to null to show Internal and External memory options*/
         mCurrentPath = null;
         if(D) Log.d(TAG,"ENV:  PRIMARY:  "+ rootPrimaryStoragePath +
